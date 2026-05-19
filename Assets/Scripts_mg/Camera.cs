3f2a7b8fb3e3378ec23e45a4e96982f375ba5a97
okaka-1205 <k.okamoto_1205@icloud.com>
2026-05-19 14:19:47 +0900

    camera2

--- Assets/Scripts_mg/Camera.cs
+++ Assets/Scripts_mg/Camera.cs
@@ -3,20 +3,22 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
- [SerializeField] private Transform target;
- 
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float minX, maxX, minY, maxY;
 
     // Update is called once per frame
     void Update()
-    {FollowTarget();
-        
+    {
+        FollowTarget();
     }
     private void FollowTarget()
     {
         if (target != null)
-        {
-            transform.position = target.position;
-            
+        {float x = Mathf.Clamp(target.position.x, minX, maxX);
+         float y = Mathf.Clamp(target.position.y, minY, maxY);
+         transform.position = new Vector3(x, y, transform.position.z);
+
         }
     }
 }
