93f1bc8b09eba9993f025f81a865e2ad3b9856b2
okaka-1205 <k.okamoto_1205@icloud.com>
2026-05-20 15:45:48 +0900

    左右差追加

--- Assets/Scripts/Player_Programming1.cs
+++ Assets/Scripts/Player_Programming1.cs
@@ -5,11 +5,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private float movespeed = 5.0f;//speed of the player movement
     [SerializeField] private float jumpforce = 5.0f;//force applied to the player when jumping
     private Rigidbody2D rb;//reference to the player's rigidbody component
+    private SpriteRenderer sr;//reference to the player's sprite renderer component
     private bool isGrounded;//check if the player is on the ground
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,8 +24,17 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         float direction = Input.GetAxisRaw("Horizontal");//get the horizontal input axis (A/D or Left/Right arrow keys)
         rb.linearVelocity = new Vector2(direction * movespeed, rb.linearVelocity.y);//set the player's velocity based on the input and movespeed
+    if (direction > 0)
+    {
+        sr.flipX = true;
+    }
+    else if (direction < 0)
+    {
+        sr.flipX = false;
+    }
     }
     private void Jump()
+    
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)//check if the space key is pressed and player is grounded
         {
@@ -33,7 +44,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        isGrounded = true;
-    }
+    {   if (collision.gameObject.CompareTag("Ground"))//check if the player collides with an object tagged as "Ground"
+        {
+        isGrounded = true;//set isGrounded to true when the player collides with the ground
+        }
+    }   
 }
